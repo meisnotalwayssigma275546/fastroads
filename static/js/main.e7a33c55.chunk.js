94ccfe3ebf2e827eedfbbd3d82010a073ab54cb4
meisnotalwayssigma275546 <legek310@isd271.org>
2026-09-16 13:11:40 -0500

    s

--- static/js/main.e7a33c55.chunk.js
+++ static/js/main.e7a33c55.chunk.js
@@ -12270,7 +12270,7 @@
             metrics: {
               shadowMapSize: 4,
               steerSpeed: 1.57,
-              accel: 43287284829748273849732987492374987328972385732987589327598,
+              accel: Number.POSITIVE_INFINITY,
               reverse: 5,
               jerk: 48,
               brake: 8,
@@ -12280,7 +12280,7 @@
               dampening: 0.08,
               rockFactor: 5,
               drag: 0.001,
-              topSpeed: 959495493658374593275932753295723597238675873267582375673285627387532,
+              topSpeed: Number.POSITIVE_INFINITY,
               rollResistance: 0.04,
               steerInterval: 1.8,
               slipBase: 1,
